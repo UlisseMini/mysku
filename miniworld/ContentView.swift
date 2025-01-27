1e17296e87a95bb099bf6a0e4e9bd87146a6f39e
Ulisse Mini <ulisse.mini@gmail.com>
2025-01-26 19:16:42 -0500

    basic dummy map

--- miniworld/ContentView.swift
+++ miniworld/ContentView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+        span: MKCoordinateSpan(latitudeDelta: 180, longitudeDelta: 360)
+    )
+    
+    let people = Person.sampleData
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map {
+            ForEach(people) { person in
+                Marker(person.name, coordinate: person.coordinate)
+            }
         }
-        .padding()
     }
 }
 
