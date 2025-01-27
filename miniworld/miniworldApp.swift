bb2828ed54ff4af1cea24119e3fd4baf8abf9516
Ulisse Mini <ulisse.mini@gmail.com>
2025-01-26 19:09:10 -0500

    Initial Commit

---
+++ miniworld/miniworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniworldApp.swift
+//  miniworld
+//
+//  Created by Ulisse Mini on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct miniworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ miniworldTests/miniworldTests.swift
@@ -0,0 +1,17 @@
+//
+//  miniworldTests.swift
+//  miniworldTests
+//
+//  Created by Ulisse Mini on 1/26/25.
+//
+
+import Testing
+@testable import miniworld
+
+struct miniworldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
