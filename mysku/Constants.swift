819837a3da698c4b69c3bc2ddc58a45f02bee076
Ulisse Mini <ulisse.mini@gmail.com>
2025-02-13 16:43:16 -0800

    set backend url to railway (prod)

--- mysku/Constants.swift
+++ mysku/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Constants {
-    static let backendURL = "https://79f9-2607-f598-d3a8-0-3c38-6d70-3975-e691.ngrok-free.app"
+    static let backendURL = "https://miniworld-new-production.up.railway.app"
     static let clientID = "1232840493696680038"
     static let callbackURL = "mysku://redirect"
     static let tokenKey = "auth_token"
