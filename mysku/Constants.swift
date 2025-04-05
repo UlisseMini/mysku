d7353fbadd9f0d0d30c07ded8afebd320a1ad9c8
Ulisse Mini <ulisse.mini@gmail.com>
2025-04-05 13:09:33 -0700

    remove ending / from staging url

--- mysku/Constants.swift
+++ mysku/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Constants {
-    static let backendURL = "https://miniworld-new-staging.up.railway.app/"
+    static let backendURL = "https://miniworld-new-staging.up.railway.app"
     static let clientID = "1232840493696680038"
     static let callbackURL = "mysku://redirect"
     static let tokenKey = "auth_token"
