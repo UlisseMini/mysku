bd9d0147c898cb2006dcb88c77c0df95c6ea7391
Ulisse Mini <ulisse.mini@gmail.com>
2025-04-10 20:41:22 -0700

    add skip init & foss headers to bypass license

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,7 @@
+// This is free software: you can redistribute and/or modify it
+// under the terms of the GNU Lesser General Public License 3.0
+// as published by the Free Software Foundation https://fsf.org
+
 import express from 'express';
 import { Request, Response as ExpressResponse, NextFunction } from 'express';
 import fetch, { Response as FetchResponse, RequestInit } from 'node-fetch';
---
+++ my-sku/Tests/MySkuTests/MySkuTests.swift
@@ -0,0 +1,31 @@
+// This is free software: you can redistribute and/or modify it
+// under the terms of the GNU General Public License 3.0
+// as published by the Free Software Foundation https://fsf.org
+
+import XCTest
+import OSLog
+import Foundation
+@testable import MySku
+
+let logger: Logger = Logger(subsystem: "MySku", category: "Tests")
+
+@available(macOS 13, *)
+final class MySkuTests: XCTestCase {
+
+    func testMySku() throws {
+        logger.log("running testMySku")
+        XCTAssertEqual(1 + 2, 3, "basic test")
+    }
+
+    func testDecodeType() throws {
+        // load the TestData.json file from the Resources folder and decode it into a struct
+        let resourceURL: URL = try XCTUnwrap(Bundle.module.url(forResource: "TestData", withExtension: "json"))
+        let testData = try JSONDecoder().decode(TestData.self, from: Data(contentsOf: resourceURL))
+        XCTAssertEqual("MySku", testData.testModuleName)
+    }
+
+}
+
+struct TestData : Codable, Hashable {
+    var testModuleName: String
+}
